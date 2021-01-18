f4b42831d479bdecea773e59cc8f45a7bda76261
Habced <Habced@users.noreply.github.com>
2021-01-18 18:50:36 +0900

    inquiry model update

--- lib/models/inquiry.model.dart
+++ lib/models/inquiry.model.dart
@@ -1,6 +1,5 @@
 class Inquiry {
   final int id;
-  final String userFullname;
   final String title;
   final String inquiry;
   List<InquiryReply> replies;
@@ -8,20 +7,18 @@ class Inquiry {
 
   Inquiry({
     this.id,
-    this.userFullname,
     this.title,
     this.inquiry,
     this.isExpanded,
+    this.replies,
   });
 }
 
 class InquiryReply {
-  final int id;
   final String userFullname;
   final int replyId;
   final String reply;
   final String createDate;
 
-  InquiryReply(
-      {this.id, this.userFullname, this.replyId, this.reply, this.createDate});
+  InquiryReply({this.userFullname, this.replyId, this.reply, this.createDate});
 }
