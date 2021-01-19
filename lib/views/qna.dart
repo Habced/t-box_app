fd6c044a2dcd71d45666bbe2f9ac50f2b8e4a0e6
Habced <Habced@users.noreply.github.com>
2021-01-19 12:06:32 +0900

    inquiry page implemented

--- lib/views/qna.dart
+++ lib/views/qna.dart
@@ -11,8 +11,10 @@ class QnaScreen extends StatefulWidget {
   QnaScreenState createState() => QnaScreenState();
 }
 
-class QnaScreenState extends State<QnaScreen> {
+class QnaScreenState extends State<QnaScreen> with SingleTickerProviderStateMixin {
   GlobalKey<ScaffoldState> _globalKey = GlobalKey();
+
+  TabController _tabController;
 
   int _uid;
 
@@ -24,14 +26,17 @@ class QnaScreenState extends State<QnaScreen> {
   @override
   void initState() {
     super.initState();
-    getFutureInquiries();
+    _tabController = TabController(vsync: this, length: 2);
+    futureInquiries = getFutureInquiries();
   }
 
-  getFutureInquiries() async {
+  Future<List<Inquiry>> getFutureInquiries() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     _uid = prefs.getInt('id') ?? -1;
     if (_uid != -1) {
-      futureInquiries = appService.getInquiryByUser(_uid, true);
+      return appService.getInquiryByUser(_uid, true);
+    } else {
+      return [];
     }
   }
 
@@ -54,127 +59,160 @@ class QnaScreenState extends State<QnaScreen> {
             maxLines: 1,
             controller: _inquiryTitleController,
             decoration: InputDecoration(
-                contentPadding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
-                hintText: "제목을 입력해주세요.",
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0))),
+              contentPadding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 5.0),
+              hintText: "제목을 입력해주세요.",
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+            ),
           ),
-          SizedBox(
-            height: 10,
-          ),
+          SizedBox(height: 10),
           Expanded(
-              child: TextField(
-            maxLines: 30,
-            controller: _inquiryController,
-            decoration: InputDecoration(
+            child: TextField(
+              maxLines: 30,
+              controller: _inquiryController,
+              decoration: InputDecoration(
                 contentPadding: EdgeInsets.fromLTRB(10.0, 8.0, 10.0, 5.0),
                 hintText: "문의하실 내용을 입력해주세요.",
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0))),
-          )),
-          SizedBox(
-            height: 30,
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+              ),
+            ),
           ),
+          SizedBox(height: 30),
           SizedBox(
             height: 40,
             width: double.infinity,
             child: FlatButton(
-                color: MyPrimaryYellowColor,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5)),
-                onPressed: () {
-                  // debugPrint("pressed");
-                  if (_inquiryController.text == "") {
-                    FlutterToast.showToast(
-                      msg: "Inquiry cannot be empty.",
-                      toastLength: Toast.LENGTH_SHORT,
+              color: MyPrimaryYellowColor,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+              onPressed: () async {
+                // debugPrint("pressed");
+                if (_uid == -1) {
+                  FlutterToast.showToast(
+                    msg: "Please log in to add an inquiry.",
+                    toastLength: Toast.LENGTH_SHORT,
+                  );
+                } else if (_inquiryTitleController.text == "") {
+                  FlutterToast.showToast(
+                    msg: "Title cannot be empty.",
+                    toastLength: Toast.LENGTH_SHORT,
+                  );
+                } else if (_inquiryController.text == "") {
+                  FlutterToast.showToast(
+                    msg: "Inquiry cannot be empty.",
+                    toastLength: Toast.LENGTH_SHORT,
+                  );
+                } else {
+                  // Call the api
+                  var result = await appService.addInquiry(_uid, _inquiryTitleController.text, _inquiryController.text);
+                  if (result['res_code'] == 1) {
+                    _inquiryTitleController.text = "";
+                    _inquiryController.text = "";
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Inquiry Successfuly Sent'),
+                          content: SingleChildScrollView(
+                            child: ListBody(
+                              children: <Widget>[
+                                Text('Your inquiry was sent to the admin. Something about the reply'),
+                              ],
+                            ),
+                          ),
+                          actions: <Widget>[
+                            FlatButton(
+                              child: Text('Close'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        );
+                      },
                     );
-                  } else {
-                    // Call the api
+                    _tabController.animateTo(1);
                   }
-                },
-                child: Text("확인")),
+                }
+              },
+              child: Text("확인"),
+            ),
           ),
         ],
       ),
     );
 
     var replyTab = Padding(
-        padding: EdgeInsets.all(20),
-        child: FutureBuilder<List<Inquiry>>(
-          future: futureInquiries,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ExpansionPanelList(
-                expansionCallback: (int index, bool isExpanded) {
-                  setState(() {
-                    snapshot.data[index].isExpanded = !isExpanded;
-                  });
-                },
-                children: snapshot.data.map<ExpansionPanel>((Inquiry inquiry) {
-                  return ExpansionPanel(
-                      headerBuilder: (BuildContext context, bool isExpanded) {
-                        return ListTile(
-                          title: Text(inquiry.inquiry),
-                          subtitle: Text(
-                              inquiry.replies.length == 0 ? "미답변" : "답변완료"),
-                        );
-                      },
-                      body: inquiry.replies.length == 0
-                          ? Container()
-                          : ListTile(
-                              title: Text(inquiry.replies[0].reply),
-                              // subtitle: ,
-                              // trailing: Icon(Icons.??)
-                              // onTap: () { setState((){})},
-                            ),
-                      isExpanded: inquiry.isExpanded,
-                      canTapOnHeader: true);
-                }).toList(),
-              );
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
+      padding: EdgeInsets.all(20),
+      child: FutureBuilder<List<Inquiry>>(
+        future: futureInquiries,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ExpansionPanelList(
+              expansionCallback: (int index, bool isExpanded) {
+                setState(() {
+                  snapshot.data[index].isExpanded = !isExpanded;
+                });
+              },
+              // children: snapshot.data.map<ExpansionPanel>((Inquiry inquiry) {
+              children: snapshot.data.map<ExpansionPanel>((Inquiry inquiry) {
+                return ExpansionPanel(
+                  headerBuilder: (BuildContext context, bool isExpanded) {
+                    return ListTile(
+                      title: Text(inquiry.inquiry),
+                      subtitle: Text(inquiry.replies.length == 0 ? "미답변" : "답변완료"),
+                    );
+                  },
+                  body: inquiry.replies.length == 0
+                      ? Container()
+                      : ListTile(
+                          title: Text(inquiry.replies[0].reply),
+                          // subtitle: ,
+                          // trailing: Icon(Icons.??)
+                          // onTap: () { setState((){})},
+                        ),
+                  isExpanded: inquiry.isExpanded,
+                  canTapOnHeader: true,
+                );
+              }).toList(),
+            );
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
 
-            return Center(
-                child: Column(
+          return Center(
+            child: Column(
               mainAxisSize: MainAxisSize.min,
-              children: [
-                CircularProgressIndicator(),
-                SizedBox(height: 10),
-                Text("Loading...")
-              ],
-            ));
-          },
-        ));
+              children: [CircularProgressIndicator(), SizedBox(height: 10), Text("Loading...")],
+            ),
+          );
+        },
+      ),
+    );
 
     return Container(
-        color: Colors.black,
-        child: DefaultTabController(
-          length: 2,
-          child: Column(
-            children: [
-              TabBar(
-                tabs: [
-                  Tab(
-                    text: "1:1 문의",
-                  ),
-                  Tab(
-                    text: "문의내역",
-                  ),
-                ],
-              ),
-              Expanded(
-                child: TabBarView(
-                  children: [
-                    inquiryTab,
-                    replyTab,
-                  ],
-                ),
-              ),
+      color: Colors.black,
+      child: Column(
+        children: [
+          TabBar(
+            controller: _tabController,
+            tabs: [
+              Tab(text: "1:1 문의"),
+              Tab(text: "문의내역"),
             ],
           ),
-        ));
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                inquiryTab,
+                SingleChildScrollView(
+                  child: replyTab,
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildInquiry(Inquiry inquiry) {
