a635b1611684caea7c6b5f50f4b829d71eca241c
Habced <Habced@users.noreply.github.com>
2021-01-20 18:21:19 +0900

    vodShort model added

--- lib/models/vod.model.dart
+++ lib/models/vod.model.dart
@@ -11,17 +11,35 @@ class Vod {
   final List<int> viewableTo;
   bool isFavorite;
 
-  Vod({
+  Vod(
+      {this.id,
+      this.title,
+      this.contents,
+      this.vod,
+      this.mrbg,
+      this.thumbnail,
+      this.primaryCate,
+      this.secondaryCate,
+      this.gangsaName,
+      this.viewableTo});
+}
+
+class VodShort {
+  final int id;
+  final String title;
+  final String contents;
+  final String thumbnail;
+  final int pcTitle;
+  final int scTitle;
+  final String timestamp;
+
+  VodShort({
     this.id,
     this.title,
     this.contents,
-    this.vod,
-    this.mrbg,
     this.thumbnail,
-    this.primaryCate,
-    this.secondaryCate,
-    this.gangsaName,
-    this.viewableTo
+    this.pcTitle,
+    this.scTitle,
+    this.timestamp,
   });
-
 }
